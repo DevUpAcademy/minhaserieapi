811fe14b698e934178ab778b790e22914f96e47e
Dikz <cicerogabriel12@hotmail.com>
2019-02-13 19:49:51 -0300

    Rota /series : Listando Top Series

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const routes = express.Router()
 const controllers = require('./app/controllers')
 
 routes.get('/serie/:term', controllers.SerieController.search)
+routes.get('/series', controllers.SerieController.top)
 
 module.exports = routes
