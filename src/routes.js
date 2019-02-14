a817ae44f69e79ef0e010bcaddf71d8402db271f
Dikz <cicerogabriel12@hotmail.com>
2019-02-14 10:45:44 -0300

    Render index - Mostrando .md na rota principal

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ const express = require('express')
 const routes = express.Router()
 const controllers = require('./app/controllers')
 
+routes.get('/', (req, res) => {
+	res.render('index');
+})
 routes.get('/search/:term', controllers.SerieController.search)
 routes.get('/serie/:name', controllers.SerieController.show)
 routes.get('/series', controllers.SerieController.top)
