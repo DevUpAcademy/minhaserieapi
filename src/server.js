2fd26bb63b6e5558b567ad6e34e09fd79e0e81b5
Dikz <cicerogabriel12@hotmail.com>
2019-02-14 10:46:40 -0300

    Configuração de Engine na Class App

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const path = require('path')
+const remarkable = require('express-remarkable')
 
 class App {
 	constructor () {
 		this.express = express()
 
+		this.engine()
 		this.routes()
+	}
+
+	engine () {
+		this.express.engine('md', remarkable(this.express));
+		this.express.set('view engine', 'md');
+		this.express.set('views', path.resolve(__dirname, 'views'))
 	}
 
 	routes () {
