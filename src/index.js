7e8fedb946e0cbd3d4c0cc4eafd872a627387c77
Dikz <cicerogabriel12@hotmail.com>
2019-02-21 12:06:12 -0300

    Textos e comportamento de inicialização

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const server = require('./server')
+const opn = require('opn')
 
 server.listen(process.env.PORT || 3000, () => {
-	console.log(`Is On!`)
+	console.log(`Servidor Online`)
+
+	if (!process.env.PORT) {
+		console.log(`Executando em: http://localhost:3000`)
+		opn(`http://localhost:3000`)
+	}
 })
