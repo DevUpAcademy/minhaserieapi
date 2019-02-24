fd086d01178d12ba891981048b61b1a845dca6c8
Dikz <cicerogabriel12@hotmail.com>
2019-02-24 02:32:38 -0300

    Uninstall opn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 const server = require('./server')
-const opn = require('opn')
 
 server.listen(process.env.PORT || 3000, () => {
 	console.log(`Servidor Online`)
-
-	if (!process.env.PORT) {
-		console.log(`Executando em: http://localhost:3000`)
-		opn(`http://localhost:3000`)
-	}
 })
