a7114e52eed54ae61a8df5c39133e9f8a01bb259
Dikz <cicerogabriel12@hotmail.com>
2019-02-27 20:40:15 -0300

    Create: Controller de Noticias + Metodo index

---
+++ src/app/controllers/NewController.js
@@ -0,0 +1,39 @@
+const rp = require('request-promise')
+const st = require('../tools/ScrapperTool')
+const urlMS = 'https://www.minhaserie.com.br'
+
+class NewController {
+	index(req, res) {
+		let options = st.setURI(`${urlMS}/novidades?page=1`)
+
+		rp(options)
+			.then(($) => {
+				let resultCount = 0;
+				let body = {
+					results: [],
+					count: ''
+				}
+
+				$('.update-list li.horizontal').each(function (i, el) {
+					resultCount++;
+
+					let noticia = {
+						"title": $(this).find('h2.info-title').text().trim(),
+						"thumb": $(this).find('.image img').attr('src'),
+						"author": $(this).find('.info-list .info-post em').text().trim()
+					}
+					body.results.push(noticia)
+				})
+
+				body.count = resultCount
+				res.json(body)
+			})
+			.catch(err => {
+				return res.status(400).send({
+					error: 'Erro na busca da serie'
+				})
+			})
+	}
+}
+
+module.exports = new NewController()
