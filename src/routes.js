b72fcf9c6efdff6f786945cbd562f5f14a14cf2f
Dikz <cicerogabriel12@hotmail.com>
2019-03-03 16:33:14 -0300

    Add: Rota /new:name

--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,6 @@ routes.get('/search/:term', controllers.SerieController.search)
 routes.get('/serie/:name', controllers.SerieController.show)
 routes.get('/series', controllers.SerieController.top)
 routes.get('/news', controllers.NewController.index)
+routes.get('/new/:name', controllers.NewController.show)
 
 module.exports = routes
