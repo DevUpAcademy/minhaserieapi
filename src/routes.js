3bb51d4adbe9871b92dea255f120b9dfd8e3e59c
Dikz <cicerogabriel12@hotmail.com>
2019-03-06 18:40:18 -0300

    Add: Rota /tags

--- src/routes.js
+++ src/routes.js
@@ -11,5 +11,6 @@ routes.get('/serie/:name', controllers.SerieController.show)
 routes.get('/series', controllers.SerieController.top)
 routes.get('/news', controllers.NewController.index)
 routes.get('/new/:name', controllers.NewController.show)
+routes.get('/tags', controllers.TagController.top)
 
 module.exports = routes
