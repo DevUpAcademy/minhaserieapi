11bdeec85bdcdc2cb93f095f8972e0ecb8cb5c92
Dikz <cicerogabriel12@hotmail.com>
2019-04-01 23:30:37 -0300

    Plural Rotas

--- src/routes.js
+++ src/routes.js
@@ -7,10 +7,10 @@ routes.get('/', (req, res) => {
 	res.render('index');
 })
 routes.get('/search/:term', controllers.SerieController.search)
-routes.get('/serie/:name', controllers.SerieController.show)
+routes.get('/series/:name', controllers.SerieController.show)
 routes.get('/series', controllers.SerieController.top)
 routes.get('/news', controllers.NewController.index)
-routes.get('/new/:name', controllers.NewController.show)
+routes.get('/news/:name', controllers.NewController.show)
 routes.get('/tags', controllers.TagController.top)
 
 module.exports = routes
